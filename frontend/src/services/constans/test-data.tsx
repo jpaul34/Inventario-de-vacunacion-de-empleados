6a18848e50ab6f02fe96f3b83b1774565199c922
Jonathan Caiza <jpaul.snp@gmail.com>
2022-09-19 23:52:18 -0500

    Update test-data.tsx

--- frontend/src/services/constans/test-data.tsx
+++ frontend/src/services/constans/test-data.tsx
@@ -2,35 +2,35 @@ import { EmployeeInterface } from "../utils/interfaces/employee.interface";
 
 export const listEmployee: EmployeeInterface[] = [
     {
-        cedula: "1734567890",
-        nombres: "Nombreuno Nombredos",
-        apellidos: "Apellidouno Apelidodos",
-        correo: "correo@mail.com",
-        fechaNacimiento: "01/01/2000",
+        cedula: "1722649926",
+        nombres: "Jonathan Paul",
+        apellidos: "Caiza Llumitaxi",
+        correo: "jonathan@mail.com",
+        fechaNacimiento: "29/03/1995",
         direccion: "Calle 7892, OE-593",
         movil: "0991234567",
         estaVacunado: false,
-        nombreUsuario: "krugeriano1734567890",
-        password: "qwerty593",
-        rol: "Empleado",
-        tipoVacuna: "",
-        fechaVacunacion: "",
-        numeroDosis: 0
-    },
-    {
-        cedula: "1734567891",
-        nombres: "Nombreuno Nombredos",
-        apellidos: "Apellidouno Apelidodos",
-        correo: "correo@mail.com",
-        fechaNacimiento: "01/01/2000",
-        direccion: "Calle 7892, OE-593",
-        movil: "0991234567",
-        estaVacunado: true,
-        nombreUsuario: "krugeriano1734567891",
-        password: "qwerty593",
+        nombreUsuario: "krugeriano1722649926",
+        password: "epn593*",
         rol: "Administrador",
         tipoVacuna: "Pfizer",
         fechaVacunacion: "2022-01-01",
         numeroDosis: 2
+    },
+    {
+        cedula: "1709959652",
+        nombres: "Paul Jonathan",
+        apellidos: "Llumitaxi Caiza",
+        correo: "paul@mail.com",
+        fechaNacimiento: "01/01/1995",
+        direccion: "Calle 7892, OE-593",
+        movil: "0991234567",
+        estaVacunado: true,
+        nombreUsuario: "krugeriano1734567891",
+        password: "epn593*",
+        rol: "Empleado",
+        tipoVacuna: "",
+        fechaVacunacion: "",
+        numeroDosis: 0
     }
 ];
